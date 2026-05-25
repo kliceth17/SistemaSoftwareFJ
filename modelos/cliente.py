89c653fa05a417c88c8f5ad52af05658c9f6496d
kliceth17 <kliceth179@gmail.com>
2026-05-24 23:00:18 -0500

    Update cliente.py

--- modelos/cliente.py
+++ modelos/cliente.py
@@ -1,1 +1,22 @@
+from modelos.entidad import Entidad
 
+class Cliente(Entidad):
+
+    def __init__(self, nombre, correo):
+        if not nombre.strip():
+            raise ValueError("El nombre no puede estar vacío")
+
+        if "@" not in correo:
+            raise ValueError("Correo inválido")
+
+        self.__nombre = nombre
+        self.__correo = correo
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_correo(self):
+        return self.__correo
+
+    def mostrar_info(self):
+        return f"Cliente: {self.__nombre} - {self.__correo}"
