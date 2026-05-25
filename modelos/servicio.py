e0dbbf35b3e1f2cb254097a25b8048c911b44300
kliceth17 <kliceth179@gmail.com>
2026-05-24 23:01:34 -0500

    Update servicio.py

--- modelos/servicio.py
+++ modelos/servicio.py
@@ -1,1 +1,18 @@
+from abc import ABC, abstractmethod
 
+class Servicio(ABC):
+
+    def __init__(self, nombre, precio_base):
+        if precio_base <= 0:
+            raise ValueError("El precio debe ser mayor a cero")
+
+        self.nombre = nombre
+        self.precio_base = precio_base
+
+    @abstractmethod
+    def calcular_costo(self):
+        pass
+
+    @abstractmethod
+    def descripcion(self):
+        pass
